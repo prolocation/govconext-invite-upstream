b78ed76d11b18d01806b0f45dfdfeb9f6d817ab6
Prolocation <beheer@prolocation.net>
2024-11-08 13:44:02 +0100

    Force version, update SDS

--- client/pushtodocker.sh
+++ client/pushtodocker.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 IMAGENAME="ghcr.io/openconext/openconext-invite/inviteclient"
+VERSION=0.0.20
 
 mvn clean install
-docker build --tag ${IMAGENAME} -f docker/Dockerfile .
+docker build --tag ${IMAGENAME}:${VERSION} -f docker/Dockerfile .
